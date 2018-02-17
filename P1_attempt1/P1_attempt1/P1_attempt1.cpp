144df1012ed39c88f866aeb8021f064b5d31f123
Bibushka <bianca_bucur03@yahoo.com>
2018-02-17 23:43:32 +0200

    P1_attemp1_4: creates output file, does not write in it or in compiler

--- P1_attempt1/P1_attempt1/P1_attempt1.cpp
+++ P1_attempt1/P1_attempt1/P1_attempt1.cpp
@@ -4,20 +4,39 @@
 #include <string>
 using namespace std;
 
-void read_write(char* argv, int i)
+void read_write(string argv, int index)
 {
+	const string fileName = argv.substr(argv.rfind("/") + 1, string::npos);
 	ifstream inputFile;
-	inputFile.open(argv, ios::in);
+	inputFile.open(fileName, ios::in);
 	ofstream outputFile;
-	outputFile.open("abcde.txt"); //outputFile.open("out_t" + std::to_string(i) + ".txt");
-	outputFile << "Hello world!";
+	outputFile.open("out_t" + std::to_string(index) + ".txt");
+	const string deviceNumber = "Nr. devices: ";
+	outputFile << "yo yo";
+	for (std::string line; getline(inputFile, line);)
+	{
+		if (line.empty())
+		{
+			getline(inputFile, line);
+			std::cout << line.substr(0, 18) << endl;
+			outputFile << line.substr(0, 18) << endl;
+		}
+		if (line.find(deviceNumber) == 0)
+		{
+			std::cout << line.substr(0, line.find(",") - 1) << endl;
+			outputFile << line.substr(0, line.find(",") - 1) << endl << endl;
+		}
+	}
 	inputFile.close();
 }
+
 
 int main(int argc, char **argv)
 {
 	for (int i = 1; i < argc; i++)
+	{
 		read_write(argv[i], i);
+		cout << endl;
+	}
 	return 0;
 }
-
